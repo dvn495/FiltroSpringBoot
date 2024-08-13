4c8ed6da35151777e6229e462ace011eba7a5e57
dvn495 <152308538+dvn495@users.noreply.github.com>
2024-08-13 10:12:56 -0500

    commit

--- examenfiltro/src/main/java/com/examenfiltro/examenfiltro/customer/infrastructure/in/customerController.java
+++ examenfiltro/src/main/java/com/examenfiltro/examenfiltro/customer/infrastructure/in/customerController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -38,7 +39,7 @@ public class customerController {
     }
     
     @PostMapping
-    public Customer createCustomer(Customer customer) {
+    public Customer createCustomer(@RequestBody Customer customer) {
         customerService.save(customer);
         return customer;
     }
---
+++ examenfiltro/target/classes/import.sql
@@ -0,0 +1,6 @@
+
+INSERT INTO examen.country(codecountry, namecountry) VALUES("P01", "COLOMBIA");
+
+INSERT INTO examen.region(codereg, namereg, codecountryregion) VALUES("R01", "SANTANDER", "P01");
+
+INSERT INTO examen.city(codecity, namecity, coderegcity) VALUES("C01", "BUCARAMANGA", "R01");
